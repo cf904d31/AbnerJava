b9e2808d348470c9b681a87db62d0583c5091a8b
cf904d31 <cf904d31@gmail.com>
2016-08-31 14:33:49 +0800

    charAt2k7

---
+++ AbnerJava20160831/src/tw/org/iii/AbnerJava/charAt.java
@@ -0,0 +1,11 @@
+package tw.org.iii.AbnerJava;
+
+public class charAt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "L123456789";
+		System.out.println(a.charAt(0));
+	}
+
+}
