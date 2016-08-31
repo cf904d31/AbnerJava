45a687149b6b5240ff67c54de9ca7603814917a7
cf904d31 <cf904d31@gmail.com>
2016-08-31 14:34:38 +0800

    charAt的API使用方法

--- AbnerJava20160831/src/tw/org/iii/AbnerJava/charAt.java
+++ AbnerJava20160831/src/tw/org/iii/AbnerJava/charAt.java
@@ -5,7 +5,7 @@ public class charAt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String a = "L123456789";
-		System.out.println(a.charAt(0));
+		System.out.println(a.charAt(3));
 	}
 
 }
