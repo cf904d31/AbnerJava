e5b8038407f9ad1e5107262621b5db9f41261cdf
cf904d31 <cf904d31@gmail.com>
2016-08-31 10:34:09 +0800

    不合理的物件導向(正常情況下無法直接設定或取得速度的值)

--- AbnerJava20160831/src/tw/org/iii/AbnerJava/Bike.java
+++ AbnerJava20160831/src/tw/org/iii/AbnerJava/Bike.java
@@ -1,5 +1,11 @@
 package tw.org.iii.AbnerJava;
 
 public class Bike {
-
+	double speed;
+	void upSpeed() {
+		speed = (speed<1?speed=1:speed*1.2);
+	}
+	void downSpeed() {
+		speed = (speed<1?speed=0:speed*0.7);
+	}
 }
--- AbnerJava20160831/src/tw/org/iii/AbnerJava/UseBike.java
+++ AbnerJava20160831/src/tw/org/iii/AbnerJava/UseBike.java
@@ -5,7 +5,10 @@ public class UseBike {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bike myBike = new Bike();
-		
+		myBike.speed = 4;
+		System.out.println(myBike.speed);
+		myBike.upSpeed();
+		System.out.println(myBike.speed);
 	}
 
 }
