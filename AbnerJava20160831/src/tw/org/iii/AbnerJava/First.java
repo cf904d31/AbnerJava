ab32fac122d384f62be50cb2f6ac709cf738eedf
cf904d31 <cf904d31@gmail.com>
2016-08-31 09:57:11 +0800

    while迴圈的使用

--- AbnerJava20160831/src/tw/org/iii/AbnerJava/First.java
+++ AbnerJava20160831/src/tw/org/iii/AbnerJava/First.java
@@ -4,7 +4,17 @@ public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int i=0;
+		while (i<10) {
+			System.out.println(i++);
+		}
+		
+		int n=1;
+		int sum=0;
+		while (n<=100) {
+			sum += n++;
+		}
+		System.out.println("1+2+3+...+"+n+"="+sum);
 	}
 
 }
