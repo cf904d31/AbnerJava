cc00a5d3b67fd55d7f62ff07ae3b6a9c1c1f77fe
cf904d31 <cf904d31@gmail.com>
2016-09-01 12:02:07 +0800

    JAVA視窗應用搭配繼承的使用

---
+++ AbnerJava20160901/src/tw/org/iii/AbnerJava/Third.java
@@ -0,0 +1,30 @@
+package tw.org.iii.AbnerJava;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+//----------繼承 javax.swing  中的     JFrame類別
+public class Third extends JFrame {
+	
+	//------宣告擁有JButton這個類別的屬性
+	private JButton open ,save ,exit;
+	
+	Third() {
+		super("Abner APP");
+		
+		open = new JButton("Open");
+		save = new JButton("Save");
+		exit = new JButton("Exit");
+		
+		
+		setSize(640, 480);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Third();
+	}
+
+}
