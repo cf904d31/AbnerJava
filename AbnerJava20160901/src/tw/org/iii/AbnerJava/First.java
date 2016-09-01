62d9ce04456e508d3214930f74d3e5277bc40372
cf904d31 <cf904d31@gmail.com>
2016-09-01 10:48:09 +0800

    猜數字1A2B大鋼完成 可以回去再增加一些功能

--- AbnerJava20160901/src/tw/org/iii/AbnerJava/First.java
+++ AbnerJava20160901/src/tw/org/iii/AbnerJava/First.java
@@ -1,10 +1,71 @@
 package tw.org.iii.AbnerJava;
 
+import javax.swing.JOptionPane;
+
 public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1234165");
+		//-------------猜數字1A2B遊戲-----------
+		//--------1.產生出4個號碼的亂數---------
+		int n = 5;
+		String answer = createAnswer(n);
+		System.out.println(answer);
+		//--------2.開始遊戲--------------------
+		
+		for (int i=0 ; i<10 ; i++) {
+		//--------3.猜數字----------------------
+			String guess = JOptionPane.showInputDialog("請輸入" + n + "個數字");
+		//--------4.比對猜的數字----------------
+			if (guess.equals(answer)) {
+				
+			}
+			String result = checkAB(guess , answer);
+			JOptionPane.showMessageDialog(null, guess+":" + result);
+		}
+		
+		//--------5.顯示結果--------------------
 	}
-
+	
+	
+	static String createAnswer(int n) {
+		int [] porkCard = new int[n];
+		for (int i=0 ; i<porkCard.length ; i++) {
+			int temp;
+			//------檢查是否重複----------  
+			//------使用do-while來判斷----
+			boolean isRepeat;
+			do {
+				isRepeat = false;
+				temp =(int)(Math.random()*10);
+				for (int j=0 ; j<i ; j++) {
+					if (temp == porkCard[j]) {
+						isRepeat = true;
+						break;
+					}
+				}
+			} while (isRepeat);
+			porkCard[i] = temp;
+		}
+		String ret = "";
+		//-----使用foreach讓陣列中的數字逐漸加入變數ret中
+		for (int p : porkCard) {
+			ret += p;
+		}
+		return ret;
+	}
+	static String checkAB(String b, String a) {
+		int A , B ;
+		A = B = 0;
+		for (int i=0 ; i<a.length() ; i++) {
+			if (b.charAt(i) == a.charAt(i)) {
+				A++;
+			} else if (a.indexOf(b.charAt(i)) != -1) {
+				B++;
+			} else {
+				
+			}
+		}
+		return A + "A" + B + "B";
+	}
 }
