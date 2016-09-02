bdca7c300b7976add150ad4a2e906f2b139c9ba7
cf904d31 <cf904d31@gmail.com>
2016-09-02 17:00:46 +0800

    FileIO進階介紹及應用

--- AbnerJava20160902/src/tw/org/iii/AbnerJava/FileInput_Output.java
+++ AbnerJava20160902/src/tw/org/iii/AbnerJava/FileInput_Output.java
@@ -1,6 +1,10 @@
 package tw.org.iii.AbnerJava;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilterInputStream;
+import java.io.IOException;
 
 public class FileInput_Output {
 
@@ -10,6 +14,45 @@ public class FileInput_Output {
 		System.out.println(file1.getAbsolutePath());
 		if (file1.exists()) {
 			System.out.println("檔案存在");
+		}
+		
+		
+		//----讀取檔案-----
+		
+		try {
+			FileInputStream fis = new FileInputStream(file1);
+			int c;
+			
+			//-----------讀取檔案方法1-----------
+			//-----一個一個字元的讀取檔案
+			//while ( (c=fis.read()) != -1 ) {
+			//	System.out.print((char)c);
+			//}
+			
+			
+			//-----------讀取檔案方法2-----------
+			//-----因為讀取不到中文字碼所以
+			//byte []buf = new byte[3];
+			//while ( (c = fis.read(buf)) != -1 ) {
+			//	System.out.print(new String(buf,0,c));
+			//}
+			
+			
+			//-----------讀取檔案方法3-----------
+			long len = file1.length();
+			byte []buf = new byte[(int)len];
+			c = fis.read(buf);
+			//System.out.println(c);
+			System.out.print(new String(buf));
+			
+			
+			fis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
