1dd8aab364e48f1f05349bdc767912e8a2a10cf9
cf904d31 <cf904d31@gmail.com>
2016-09-06 15:08:13 +0800

    執行序搭配賽車遊戲實作

---
+++ AbnerJava20160906/src/tw/org/iii/AbnerJava/Thread_Practice.java
@@ -0,0 +1,82 @@
+package tw.org.iii.AbnerJava;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class Thread_Practice extends JFrame {
+	private JButton go;
+	private JLabel[] lanes;
+	private int rank;
+	
+	public Thread_Practice() {
+		super("Racing");
+		setLayout(new GridLayout(9, 1));
+		
+		go = new JButton("Go");
+		add(go);
+		
+		lanes = new JLabel[8];
+		for (int i=0 ; i<lanes.length ; i++) {
+			lanes[i] = new JLabel((i+1)+".");
+			add(lanes[i]);
+		}
+		
+		go.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				go();
+			}
+		});
+		
+		setSize(1024,400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
+	private void go() {
+		rank = 0;
+		Car[] cars = new Car[8];
+		for (int i=0 ; i<cars.length ; i++) {
+			cars[i] = new Car(i);
+		}
+		for (int i=0 ; i<cars.length ; i++) {
+			cars[i].start();
+		}
+	}
+	
+	private class Car extends Thread {
+		private int num;
+		Car (int num) {
+			this.num = num;
+		}
+		@Override
+		public void run() {
+			for (int i=0 ; i<120 ; i++) {
+				lanes[num].setText(lanes[num].getText()+">");
+				if (i==119) {
+					lanes[num].setText(lanes[num].getText()+ ++rank);
+				}
+				try {
+					Thread.sleep((int)(Math.random()*200));
+				} catch (InterruptedException e) {
+					
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Thread_Practice();
+	}
+
+}
