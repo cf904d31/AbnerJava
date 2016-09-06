f1f2d6436340cc0a351bc01b195246ca8247d6f7
cf904d31 <cf904d31@gmail.com>
2016-09-06 10:15:29 +0800

    畫圖程式(綜合前面所教的物件導向觀念)20160906完成可畫條線

--- AbnerJava20160905/src/tw/org/iii/AbnerJava/Drawer.java
+++ AbnerJava20160905/src/tw/org/iii/AbnerJava/Drawer.java
@@ -16,14 +16,14 @@ import com.sun.xml.internal.ws.wsdl.writer.document.http.Address;
 
 public class Drawer extends JPanel{
 	//-----<泛型>  意即  指定型別給它  讓他在編譯時就可以檢查到，不用到RunTime時再檢查
-	private LinkedList<HashMap<String,Integer>> line;
+	private LinkedList<LinkedList<HashMap<String,Integer>>> lines;
 	public Drawer() {
 		MyListener listener = new MyListener();
 		addMouseListener(listener);
 		addMouseMotionListener(listener);
 		
 		//-----將定義的LinkedList  new出來使用
-		line = new LinkedList<>();
+		lines = new LinkedList<>();
 	}
 	
 	@Override
@@ -39,14 +39,16 @@ public class Drawer extends JPanel{
 		g2d.setStroke(new BasicStroke(4));
 		//g2d.drawLine(0, 0, 100, 100);
 		
-		for (int i=1 ; i<line.size() ; i++) {
-			HashMap<String,Integer> p0 = line.get(i-1);
-			HashMap<String,Integer> p1 = line.get(i);
-			Integer p0x = p0.get("x");
-			Integer p0y = p0.get("y");
-			Integer p1x = p1.get("x");
-			Integer p1y = p1.get("y");
-			g2d.drawLine(p0x, p0y, p1x, p1y);
+		for (LinkedList<HashMap<String,Integer>> line : lines) {
+			for (int i=1 ; i<line.size() ; i++) {
+				HashMap<String,Integer> p0 = line.get(i-1);
+				HashMap<String,Integer> p1 = line.get(i);
+				Integer p0x = p0.get("x");
+				Integer p0y = p0.get("y");
+				Integer p1x = p1.get("x");
+				Integer p1y = p1.get("y");
+				g2d.drawLine(p0x, p0y, p1x, p1y);
+			}
 		}
 	}
 	
@@ -62,7 +64,7 @@ public class Drawer extends JPanel{
 			HashMap<String,Integer> point = new HashMap();
 			point.put("x", x);
 			point.put("y", y);
-			line.add(point);
+			lines.getLast().add(point);
 			
 			//-----呼叫repaint()讓他重畫repaint()是 繼承至父類別JPanel中的方法
 			repaint();
@@ -76,12 +78,16 @@ public class Drawer extends JPanel{
 			int y = arg0.getY();
 			//System.out.println("Pressed:(X=" + x + ",Y=" + y + ")");
 			
+			LinkedList<HashMap<String,Integer>> line = new LinkedList<>();
+			
 			//-----new出一個HashMap將x及y的值放進去
 			HashMap<String,Integer> point = new HashMap();
 			point.put("x", x);
 			point.put("y", y);
 			//-----將HashMap的值放入LinkedList中
 			line.add(point);
+			//-----將一條一條的線段放入lines中
+			lines.add(line);
 		}
 
 		@Override
