f4d9e3f8502b94dd70d661f32500ad3f89cacbe8
cf904d31 <cf904d31@gmail.com>
2016-09-08 13:36:38 +0800

    JavaNetUrl撈出網頁原始碼

---
+++ AbnerJava20160908/src/tw/org/iii/AbnerJava/JavaNetURL.java
@@ -0,0 +1,39 @@
+package tw.org.iii.AbnerJava;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
+public class JavaNetURL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			URL url = new URL("http://www.iii.org.tw");
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.connect();
+			
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(
+							conn.getInputStream()));
+			String line;
+			while ( (line = reader.readLine()) != null ) {
+				System.out.println(line);
+			}
+			reader.close();
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
